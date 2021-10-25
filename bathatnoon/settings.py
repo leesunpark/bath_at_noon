ad3f44a84c479c94c4c5bc0a7276cb1d9804ace9
leesunpark <leesunpark90@gmail.com>
2021-10-26 05:11:48 +0900

    Update ALLOWED_HOSTS with site and development server URL

--- bathatnoon/settings.py
+++ bathatnoon/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag"
+# SECRET_KEY = 'django-insecure-v($)=a06*w9a)*3m$bpwees(&@dh-df49ufe@5qqpmbow_&r=p'"
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['secret-scrubland-87856.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
